3265f63055813caaad7052e1121fc1b51ef38ff5
moviewatcher1946-hue <moviewatcher1946@gmail.com>
2026-08-10 13:16:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def stream():
     )
 
     return Response(
-        r.iter_content(chunk_size=240),
+        r.iter_content(chunk_size=120),
         content_type="multipart/x-mixed-replace; boundary=frame"
     )
 
